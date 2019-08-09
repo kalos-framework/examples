6a5fcc5cb59fee0619349eaa9d49eaa28304ca29
Pete Houston <contact@petehouston.com>
2019-08-09 00:16:13 -0500

    Revert "Add hello-world"
    This reverts commit 396777f8ecff4b00f07ed0a4d0983ca2884db305.

--- 1_hello-world/index.js
+++
@@ -1,9 +0,0 @@
-import Kalos from 'kalos';
-
-const server = new Kalos.Server()
-    .get('/', (req, res) => {
-        res.end('Hello World from Kalos!');
-    })
-    .start((ip, port) => {
-        console.log('Server started at ' + ip + ':' + port)
-    });
